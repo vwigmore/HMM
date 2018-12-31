4dd814acea056ecb0e7e19014f04ea8d593d615c
Viktor <vwigore@student.tudelft.nl>
2018-12-31 15:41:14 +0100

    K-fold

--- ExtHMM.py
+++ ExtHMM.py
@@ -1,11 +1,89 @@
 import seqlearn.hmm
+import numpy as np
+
+from sklearn.externals import six
+from seqlearn._utils import atleast2d_or_csr, safe_sparse_dot, validate_lengths
+
 
 class ExtHMM(seqlearn.hmm.MultinomialHMM):
 
+    def __init__(self):
+        super(ExtHMM, self).__init__(alpha=0.01)
+
     def predict(self, X, lengths=None):
-        return "test"
+        X = atleast2d_or_csr(X)
+        scores = safe_sparse_dot(X, self.coef_.T)
+        if hasattr(self, "coef_trans_"):
+            n_classes = len(self.classes_)
+            coef_t = self.coef_trans_.T.reshape(-1, self.coef_trans_.shape[-1])
+            trans_scores = safe_sparse_dot(X, coef_t.T)
+            trans_scores = trans_scores.reshape(-1, n_classes, n_classes)
+        else:
+            trans_scores = None
+
+        if lengths is None:
+            y = viterbi(scores, trans_scores, self.intercept_trans_,
+                       self.intercept_init_[0], self.intercept_final_[0])
+
+        else:
+            start, end = validate_lengths(X.shape[0], lengths)
+            y = [viterbi(scores[start[i]:end[i]], trans_scores,
+                        self.intercept_trans_, self.intercept_init_[0],
+                        self.intercept_final_[0])
+                 for i in six.moves.xrange(len(lengths))]
+            y = np.hstack(y)
+
+        return self.classes_[y]
 
 
-if __name__ == '__main__':
+def viterbi(score, trans_score, b_trans, init, final):
+    """First-order Viterbi algorithm.
+    Parameters
+    ----------
+    score : array, shape = (n_samples, n_states)
+        Scores per sample/class combination; in a linear model, X * w.T.
+        May be overwritten.
+    trans_score : array, shape = (n_samples, n_states, n_states), optional
+        Scores per sample/transition combination.
+    trans : array, shape = (n_states, n_states)
+        Transition weights.
+    init : array, shape = (n_states,)
+    final : array, shape = (n_states,)
+        Initial and final state weights.
+    References
+    ----------
+    L. R. Rabiner (1989). A tutorial on hidden Markov models and selected
+    applications in speech recognition. Proc. IEEE 77(2):257-286.
+    """
+    n_samples, n_states = score.shape[0], score.shape[1]
+    backp = np.empty((n_samples, n_states), dtype=np.intp)
 
-    print(ExtHMM().predict(""))
+    for j in range(n_states):
+        score[0][j] += init[j]
+
+    # Forward recursion. score is reused as the DP table.
+    for i in range(1, n_samples):
+        for k in range(n_states):
+            maxind = 0
+            maxval = float("-inf")
+            for j in range(n_states):
+                candidate = score[i - 1][j] + b_trans[j][k] + score[i][k]
+                if trans_score is not None:
+                    candidate += trans_score[i][j][k]
+                if candidate > maxval:
+                    maxind = j
+                    maxval = candidate
+
+            score[i][k] = maxval
+            backp[i][k] = maxind
+
+    for j in range(n_states):
+        score[n_samples - 1][j] += final[j]
+
+    # Path backtracking
+    path = np.empty(n_samples, dtype=np.intp)
+    path[n_samples - 1] = score[n_samples - 1, :].argmax()
+
+    for i in range(n_samples - 2, -1, -1):
+        path[i] = backp[i + 1][path[i + 1]]
+    return path
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import json
 import numpy as np
+import glob, os
+from ExtHMM import ExtHMM
 from UtilityFunction import *
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+
 
 # Hardcoded set of possible strategy labels
 labels = ["conceder", "random", "hardheaded", "tft"]
@@ -17,63 +20,126 @@ def parseAgentLabels(file_name):
 		if label in agents[1]:
 			agent2_label = label
 	return (agent1_label, agent2_label)
- 
-# features X: own utility, opponent utility, chosen discrete values 
-# y labels categorically encoded
-def loadTrainingData(file_name):
-	(agent1_label, agent2_label) = parseAgentLabels(file_name)
-	
-	# Read the JSON file
+
+def loadTestData(file_name):
+
 	with open(file_name, "r") as file:
 		data = json.load(file)
 		num_bids = len(data['bids'])
 		num_issues = len(data['issues'])
 		issues = setupIssues(data['issues'])
-		
+
 		# Normalizing utility function from JSON
 		utility1 = normalizeUtilityFunction(data['Utility1'])
 		utility2 = normalizeUtilityFunction(data['Utility2'])
-		
+
 		# Set up data structures to store the features in
 		# Last bid is the accept or end of negotiation
 		agent1_strat_utilities = np.empty((num_bids - 1, 2))
 		agent1_strat_bids = np.empty((num_bids - 1, num_issues), dtype=object)
-		agent1_y = np.full((num_bids - 1), agent1_label)
 		agent2_strat_utilities = np.empty((num_bids - 1, 2))
 		agent2_strat_bids = np.empty((num_bids - 1, num_issues), dtype=object)
-		agent2_y = np.full((num_bids - 1), agent2_label)
-		
+
 		for index, bid in enumerate(data['bids']):
 			if index >= num_bids - 1:
 				break
-				
 			agent1_bid = bid['agent1'].split(',')
 			agent1_strat_utilities[index, 0] = computeUtility(agent1_bid, utility1)
 			agent1_strat_utilities[index, 1] = computeUtility(agent1_bid, utility2)
 			agent1_strat_bids[index, :] = agent1_bid
-			
+
 			agent2_bid = bid['agent2'].split(',')
 			agent2_strat_utilities[index, 0] = computeUtility(agent2_bid, utility2)
 			agent2_strat_utilities[index, 1] = computeUtility(agent2_bid, utility1)
 			agent2_strat_bids[index, :] = agent2_bid
-		
-		
-		# One hot encode the categorical bids 
+
+		# One hot encode the categorical bids
 		X_bids = np.append(agent1_strat_bids, agent2_strat_bids, axis=0)
 		one_hot_enc = OneHotEncoder(categories=issues)
 		X_bids = one_hot_enc.fit_transform(X_bids).toarray()
-		
+
 		# Append the feature matrices
 		X = np.append(agent1_strat_utilities, agent2_strat_utilities, axis=0)
 		X = np.append(X, X_bids, axis=1)
+
+		return (X, [index, index])
+
+
+# features X: own utility, opponent utility, chosen discrete values 
+# y labels categorically encoded
+def loadTrainingData(dir_name, remove_file=None):
+
+	# os.chdir("~./"+dir_name)
+	files = []
+	for f in glob.glob("*.json"):
+		files.append(f)
+	if remove_file:
+		files.remove(remove_file)
+
+	# initiate arrays
+	X_bids = []
+	X = []
+	y = []
+	lengths = []
+	for f in files:
+		(agent1_label, agent2_label) = parseAgentLabels(f)
+		# Read the JSON file
+		with open(f, "r") as file:
+			data = json.load(file)
+			num_bids = len(data['bids'])
+			num_issues = len(data['issues'])
+			issues = setupIssues(data['issues'])
 		
-		# Label encode the ground truth labels
-		y = np.append(agent1_y, agent2_y, axis=0)
-		label_enc = LabelEncoder()
-		label_enc.classes_ = labels
-		y = label_enc.fit_transform(y)
+			# Normalizing utility function from JSON
+			utility1 = normalizeUtilityFunction(data['Utility1'])
+			utility2 = normalizeUtilityFunction(data['Utility2'])
 		
-		return (X, y)
+			# Set up data structures to store the features in
+			# Last bid is the accept or end of negotiation
+			agent1_strat_utilities = np.empty((num_bids - 1, 2))
+			agent1_strat_bids = np.empty((num_bids - 1, num_issues), dtype=object)
+			agent1_y = np.full((num_bids - 1), agent1_label)
+			agent2_strat_utilities = np.empty((num_bids - 1, 2))
+			agent2_strat_bids = np.empty((num_bids - 1, num_issues), dtype=object)
+			agent2_y = np.full((num_bids - 1), agent2_label)
+		
+			for index, bid in enumerate(data['bids']):
+				if index >= num_bids - 1:
+					break
+				agent1_bid = bid['agent1'].split(',')
+				agent1_strat_utilities[index, 0] = computeUtility(agent1_bid, utility1)
+				agent1_strat_utilities[index, 1] = computeUtility(agent1_bid, utility2)
+				agent1_strat_bids[index, :] = agent1_bid
+
+				agent2_bid = bid['agent2'].split(',')
+				agent2_strat_utilities[index, 0] = computeUtility(agent2_bid, utility2)
+				agent2_strat_utilities[index, 1] = computeUtility(agent2_bid, utility1)
+				agent2_strat_bids[index, :] = agent2_bid
+
+			X_bids = np.append(X_bids, agent1_strat_bids, axis=0) if len(X_bids) else agent1_strat_bids
+			X_bids = np.append(X_bids, agent2_strat_bids, axis=0)
+
+			X = np.append(X, agent1_strat_utilities, axis=0) if len(X) else agent1_strat_utilities
+			X = np.append(X, agent2_strat_utilities, axis=0)
+
+			y = np.append(y, agent1_y, axis=0) if len(y) else agent1_y
+			y = np.append(y, agent2_y, axis=0)
+
+			lengths.append(index)
+			lengths.append(index)
+
+	# One hot encode the categorical bids
+	one_hot_enc = OneHotEncoder(categories=issues)
+	X_bids = one_hot_enc.fit_transform(X_bids).toarray()
+		
+	# Append the feature matrices
+	X = np.append(X, X_bids, axis=1)
+		
+	# Label encode the ground truth labels
+	label_enc = LabelEncoder()
+	label_enc.classes_ = labels
+	y = label_enc.fit_transform(y)
+	return (X, y, lengths, label_enc)
 		
 # receives issues dictionary from JSON and sets it up for Scikit-learn one hot encoder
 def setupIssues(issues):
@@ -83,13 +149,50 @@ def setupIssues(issues):
 	return categories
 
 def main():
-   (X, y) = loadTrainingData("train/conceder_conceder.json")
-   #Use one hot encoder
-   print(X)
-   print(y)
-   #Normalize
-   
-   #Input into HMM
+
+	# k-fold cross validation
+	os.chdir("./train")
+	files = []
+	for f in glob.glob("*.json"):
+		files.append(f)
+	for f in files:
+
+		(X, y, length, label_enc) = loadTrainingData("train", f)
+		# Use one hot encoder
+		# print(X)
+		# print(y)
+		# print(length)
+		# Normalize
+
+		# Input into HMM
+		hmm = ExtHMM()
+		hmm.fit(X, y, [length])
+
+		# without directory because of changed dir in
+		(T, Tlength) = loadTestData(f)
+		classes = hmm.predict(T, Tlength)
+
+
+		print "file_name:", f
+		print "agent1:"
+		count1 = [0.0, 0.0, 0.0, 0.0]
+		for i in range(0, len(classes)/2):
+			count1[classes[i]] += 1
+		for i in range(0, len(count1)):
+			count1[i] = round(count1[i] * 200.0 / len(classes), 3)
+			print(label_enc.classes_[i], count1[i])
+
+		# print("CLASSES", classes)
+		# print(len(classes))
+		print "agent2:"
+		count2 = [0.0, 0.0, 0.0, 0.0]
+		for i in range(len(classes)/2, len(classes)):
+			count2[classes[i]] += 1
+		print(count2)
+		for i in range(0, len(count2)):
+			count2[i] = round(count2[i] * 200.0 / len(classes), 3)
+			print(label_enc.classes_[i], count2[i])
+		print
    
 
 if __name__ == "__main__":
